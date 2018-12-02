2cb648bad6894c5e832599bf48ab0908dcb51984
minling <mzhao725@gmail.com>
2018-12-02 17:55:46 -0500

    convert to pg database

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2018_11_25_033701) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "links", force: :cascade do |t|
     t.string "url"
     t.string "short_url"
