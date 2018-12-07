9c1630ec21086c2b7fa2bbb5ac0ca58061c9dd52
minling <mzhao725@gmail.com>
2018-12-06 21:21:04 -0500

    removed manual headers in controller, and adds root route

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -1,5 +1,4 @@
 class LinksController < ApplicationController
-  after_action :set_response_header
 
   def show
     link = Link.where(short_url: params['path']).first
@@ -26,12 +25,5 @@ class LinksController < ApplicationController
     top_links = links.map {|link| {url: link.url, short_url: link.short_url, title: link.title, access_count: link.access_count}}
     render json: {top_100: top_links}
   end
-
-protected
-  def set_response_header
-    headers['Access-Control-Allow-Origin'] = '*'
-    headers['Access-Control-Allow-Methods'] = 'GET,POST,OPTIONS'
-  end
-
   
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root to: 'links#index'
   get '/top', to: 'links#index'
   get '/*path', to: 'links#show'
   post '/*url', to: 'links#create'
